b34123284c9ee78bd0ef2051d80d88e03c3509b3
benjamin <benjameslindley@gmail.com>
2023-03-22 04:23:55 +0000

    notify user of errors

--- cognix.py
+++ cognix.py
@@ -2,6 +2,15 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+def validate_json(json_data):
+    errors = []
+
+    if 'uuid' in json_data:
+        errors.append("'uuid' is a special key. delete or rename this field")
+
+    return errors if errors else None
+
+
 @app.route('/')
 def index():
     return render_template('json_editor.html')
@@ -9,9 +18,15 @@ def index():
 @app.route('/process-json', methods=['POST'])
 def process_json():
     json_data = request.json
-    # Perform server-side processing on json_data as needed
-    print(json_data)
-    return jsonify(success=True)
+    errors = validate_json(json_data)
+
+    if errors:
+        return jsonify(success=False, errors=errors)
+    else:
+        # Perform server-side processing on json_data as needed
+        print(json_data)
+        return jsonify(success=True)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
